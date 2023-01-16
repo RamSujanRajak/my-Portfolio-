2d0ba561eae0d60359cf203c2bb270a4128dfc83
Ram Sujan Rajak <90238445+RamSujanRajak@users.noreply.github.com>
2023-01-16 23:19:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+
+function sendmail(){
+    
+    var params = {
+        name  : document.getElementById('name').value,
+         mail  : document.getElementById('mail').value,
+         number  : document.getElementById('number').value,
+         subject  : document.getElementById('subject').value,
+         message  : document.getElementById('message').value
+    };
+
+const serviceID = "service_xjnud9c";
+const templateID="template_lbsmnoc"
+
+emailjs
+.send(serviceID, templateID, params)
+.then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("mail").value = "";
+        document.getElementById("number").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your Message Sent Successfully");
+    })
+    .catch((err)=>console.log(err));
+}
+
+
+
+/*
+const sections = document.querySelectorAll('.section');
+const sectbtns = document.querySelectorAll('.controlls');
+const sectbtn = document.querySelectorAll('.control');
+const allsections = document.querySelectorAll('.main-content');
+
+function pageTransitions(){
+    //button click active class
+    for(let i=0; i<sectbtn.length; i++){
+        sectbtn[i].addEventListener('click', function(){
+            let currentbtn = document.querySelectorAll('.active-btn')
+            currentbtn[0].className = currentbtn[0].className.replace(' active-btn','')
+            this.className += ' active-btn'
+        })
+    }
+
+    //Section Active Class
+    allsections.addEventListener('click', (e) =>{
+        const id =e.target.dataset.id;
+        if(id){
+            //remove selected from the other 
+            sectbtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+            
+            //hide other sections 
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active')
+        }
+    })
+
+}
+
+pageTransitions():*/
